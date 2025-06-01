yet another fix of the normalization function

src/numfoil/geometry/airfoil.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 import scipy.interpolate as si
 import scipy.optimize as opt
+import scipy.integrate as spi
 
 from functools import cached_property
 from typing import Union, Tuple
@@ -180,6 +181,14 @@ class AirfoilBase(ABC):
         return self.camber_line.evaluate_at(result.x[0])
 
     @cached_property
+    def area(self) -> float:
+        """Calculates the area of the airfoil."""
+        x = cosine_spacing(0, 1, num=1000)
+        t = self.thickness_at(x)
+        return np.trapz(t, x)
+        # return spi.simpson(t, x)
+
+    @cached_property
     def leading_edge_radius(self) -> np.ndarray:
         """Returns the leading edge radius of the airfoil."""
         return self.surface.radius_at(0.5)
@@ -310,7 +319,7 @@ class BsplineAirfoil(AirfoilBase):
         # the shortened name of the airfoil, usually the filename
         self.name = name
         # the full name of the airfoil, usually from the file header
-        self.description = description or name
+        self.description = description.replace(' AIRFOIL', '') or name
 
         self.u_leading_edge = 0.5
 
@@ -553,7 +562,7 @@ class BezierAirfoil(AirfoilBase):
         # the shortened name of the airfoil, usually the filename
         self.name = name
         # the full name of the airfoil, usually from the file header
-        self.description = description or name
+        self.description = description.replace(' AIRFOIL', '') if description is not None else name
 
         self.u_leading_edge = 0.5
 
@@ -596,7 +605,13 @@ class BezierAirfoil(AirfoilBase):
         description: str = None,
         normalize: bool = True,
         fit_method: str = "split_u_l",
-        **kwargs: dict
+        n_control_points: int = None,
+        control_point_spacing=cosine_spacing(0, 1, 13)[:-1],
+        end_clamp: str | np.ndarray = "data",
+        trailing_edge_thickness: float | None = None,
+        damping_type: str = "deriv",
+        w_damping: float = 1e-1,
+        find_trailing_edge: bool = True,
     ):
         """Creates an Airfoil object from an array of points.
 
@@ -648,7 +663,7 @@ class BezierAirfoil(AirfoilBase):
         # To improve the fitting of the new spline, points are resampled from
         # the normalized bspline
         if normalize:
-            normalized_bspline = AirfoilNormalizer.normalize(points)
+            normalized_bspline = AirfoilNormalizer.normalize(points, find_trailing_edge=find_trailing_edge)
             points = AirfoilNormalizer._remove_overshoots(
                 normalized_bspline.evaluate_at(
                     np.hstack([
@@ -660,68 +675,52 @@ class BezierAirfoil(AirfoilBase):
 
         # TODO : clean this up, there must be a better way than this mess...
         match fit_method:
-            case "full":
-                # Use the composite bezier class to fit the entire curve at once
-                surfacespline = SplevCBezier.fit(
-                    points,
-                    kwargs.get("n_control_points", 12),
-                    spacing=kwargs.get("spacing", "linear"),
-                    verbose=False,
-                    w_damping=kwargs.get("w_damping", 1e-3),
-                )
-                return cls(
-                    surfacespline,
-                    name=name,
-                    description=description,
-                )
             case "split_u_l":
                 # split the points in upper and lower and fit those seperatly
                 # prefered method as it is much faster
                 upper = SplevBezier.fit(
                     points[len(points)//2::-1],
-                    n_control_points=kwargs.get("n_control_points", None),
-                    spacing=kwargs.get("spacing", np.linspace(0,1,kwargs.get("n_control_points", 12)+1)[:-1]),
+                    n_control_points=n_control_points,
+                    spacing=control_point_spacing,
                     start_clamp='origin',
-                    end_clamp=kwargs.get(
-                        "end_clamp",
-                        np.array([1.0, kwargs.get("trailing_edge_thickness", 0.002)/2])
-                    ),
-                    damping_type=kwargs.get("damping_type", "deriv"),
-                    w_damping=kwargs.get("w_damping", 1e-1),
-                    method="SLSQP",
-                    verbose=kwargs.get("verbose", False),
+                    end_clamp=end_clamp if trailing_edge_thickness is None else np.array([1.0, trailing_edge_thickness/2]),
+                    damping_type=damping_type,
+                    w_damping=w_damping,
                     constraints=[
+                        # {   # ensure all control points have positive y-values
+                        #     "type": "ineq",
+                        #     "fun": lambda y: y
+                        # },
+                        {   # force y>0.005 for first control point after the LE
+                            "type": "ineq",
+                            "fun": lambda y: y[0] - 0.002
+                        },
                         {   # ensure rounded leading edge (see GOE440)
                             "type": "ineq",
                             "fun": lambda y: y[1] - y[0]*0.5
-                        },
-                        {   # force y>0.002 for first control point after the LE
-                            "type": "ineq",
-                            "fun": lambda y: y[0] - 0.002
                         },
                     ]
                 )
                 lower = SplevBezier.fit(
                     points[len(points)//2:],
-                    n_control_points=kwargs.get("n_control_points", None),
-                    spacing=kwargs.get("spacing", np.linspace(0,1,kwargs.get("n_control_points", 13)+1)[:-1]),
+                    n_control_points=n_control_points,
+                    spacing=control_point_spacing,
                     start_clamp='origin',
-                    end_clamp=kwargs.get(
-                        "end_clamp",
-                        np.array([1.0, -kwargs.get("trailing_edge_thickness", 0.002)/2])
-                    ),
-                    damping_type=kwargs.get("damping_type", "deriv"),
-                    w_damping=kwargs.get("w_damping", 1e-1),
-                    method="SLSQP",
-                    verbose=kwargs.get("verbose", False),
+                    end_clamp=end_clamp if trailing_edge_thickness is None else np.array([1.0, -trailing_edge_thickness/2]),
+                    damping_type=damping_type,
+                    w_damping=w_damping,
                     constraints=[
+                        # {   # ensure all control points have negative y-values
+                        #     "type": "ineq",
+                        #     "fun": lambda y: -y
+                        # },
+                        {   # force y>0.005 for first control point after the LE
+                            "type": "ineq",
+                            "fun": lambda y: -y[0] + 0.002
+                        },
                         {   # ensure rounded leading edge (see GOE440)
                             "type": "ineq",
                             "fun": lambda y: -y[1] + y[0]*0.5
-                        },
-                        {   # force y>0.002 for first control point after the LE
-                            "type": "ineq",
-                            "fun": lambda y: -y[0] + 0.002
                         },
                     ]
                 )
@@ -748,29 +747,25 @@ class BezierAirfoil(AirfoilBase):
                 )
                 thickness_distribution = SplevBezier.fit(
                     thickness_pts,
-                    n_control_points=kwargs.get("n_control_points", None),
-                    spacing=kwargs.get(
-                        "spacing",
-                        np.linspace(0, 1, kwargs.get("n_control_points", 13)+1)[1:-1]
-                    ),
+                    n_control_points=n_control_points,
+                    spacing=control_point_spacing,
                     start_clamp='origin',
-                    end_clamp=np.array([1.0, kwargs.get("trailing_edge_thickness", 0.002)]),
-                    damping_type=kwargs.get("damping_type", "deriv"),
-                    w_damping=kwargs.get("w_damping", 1e-1),
-                    verbose=kwargs.get("verbose", False),
+                    end_clamp=end_clamp,
+                    damping_type=damping_type,
+                    w_damping=w_damping,
                     constraints=[
                         {   # ensure all control points have positive y-values
                             "type": "ineq",
                             "fun": lambda y: y
                         },
-                        {   # ensure rounded leading edge (see GOE440)
-                            "type": "ineq",
-                            "fun": lambda y: y[0] - 0.005
-                        },
-                        {   # ensure rounded leading edge (see GOE440)
-                            "type": "ineq",
-                            "fun": lambda y: y[1] - y[0]*0.5
-                        },
+                        # {   # ensure rounded leading edge (see GOE440)
+                        #     "type": "ineq",
+                        #     "fun": lambda y: y[0] - 0.005
+                        # },
+                        # {   # ensure rounded leading edge (see GOE440)
+                        #     "type": "ineq",
+                        #     "fun": lambda y: y[1] - y[0]*0.5
+                        # },
                     ]
                 )
                 camber_pts = bspline_airfoil.camber_line.evaluate_at(
@@ -778,17 +773,12 @@ class BezierAirfoil(AirfoilBase):
                 )
                 camber_curve = SplevBezier.fit(
                     camber_pts,
-                    n_control_points=kwargs.get("n_control_points", None),
-                    spacing=kwargs.get(
-                        "spacing",
-                        np.linspace(0,1,kwargs.get("n_control_points", 13)+1)[:-1]
-                    )[1:],
-                    start_clamp=np.array([[0.0, 0.0],[0.0, 0.0]]),#'origin', #
-                    # start_clamp='origin', #
-                    end_clamp=np.array([1.0, 0.0]),
-                    damping_type=kwargs.get("damping_type", "deriv"),
-                    w_damping=kwargs.get("w_damping", 1e-1),
-                    verbose=kwargs.get("verbose", False),
+                    n_control_points=n_control_points,
+                    spacing=control_point_spacing,
+                    start_clamp='origin',
+                    end_clamp=end_clamp,
+                    damping_type=damping_type,
+                    w_damping=w_damping,
                     constraints=[
                         # {
                         #     "type": "ineq",
@@ -807,17 +797,22 @@ class BezierAirfoil(AirfoilBase):
                     description=description,
                     points=points,
                 )
+            case "full":  # ! this one is a bad idea, as it is very slow
+                # Use the composite bezier class to fit the entire curve at once
+                surfacespline = SplevCBezier.fit(
+                    points,
+                    n_control_points,
+                    spacing=control_point_spacing,
+                    w_damping=w_damping,
+                )
+                return cls(
+                    surfacespline,
+                    name=name,
+                    description=description,
+                )
             case _:
                 raise ValueError(f"Unknown fit method: {fit_method}")
-        # add original points again for reference
-        # surfacespline.data_points = normalized_bspline.points
 
-        # return cls(
-        #     surfacespline,
-        #     name=name,
-        #     description=description,
-        #     # data_points=new_points,
-        # )
 
     @classmethod
     def from_file(

src/numfoil/geometry/data.py
@@ -119,15 +119,15 @@ class AirfoilNormalizer:
     """
 
     @classmethod
-    def normalize(cls, data: np.ndarray | BSpline2D, output="spline") -> BSpline2D:
+    def normalize(cls, data: np.ndarray | BSpline2D, output="spline", find_trailing_edge: bool = True) -> BSpline2D:
         if isinstance(data, BSpline2D):
-            return cls._normalize_spline(data)
+            return cls._normalize_spline(data, find_trailing_edge=find_trailing_edge)
         elif isinstance(data, np.ndarray):
             points = cls._remove_consecutive_duplicates(data)
             spline = BSpline2D(points)
             match output:
                 case "spline":
-                    return cls._normalize_spline(spline)
+                    return cls._normalize_spline(spline, find_trailing_edge=find_trailing_edge)
                 case "points":
                     leading_edge, trailing_edge, _ = cls._find_leading_trailing_edges(spline)
                     scale, translation, rotation = cls._compute_transformation(leading_edge, trailing_edge)
@@ -152,6 +152,9 @@ class AirfoilNormalizer:
         spline.spline[1] = cls._apply_transformation(
             spline.spline[1], scale, translation, rotation
         )
+
+        # ensure the trailing edge is correct after rotation
+        spline = cls._force_trailing_edge_at_x1(spline, target="1")
 
         # recompute
         leading_edge, trailing_edge, u_leading_edge = cls._find_leading_trailing_edges(spline,  find_trailing_edge=find_trailing_edge)
@@ -206,8 +209,8 @@ class AirfoilNormalizer:
         """Removes points that are outside the range x=[0, 1]."""
         return points[(points[:, 0] >= 0) & (points[:, 0] <= 1)]
 
-    @staticmethod
-    def _find_trailing_edge(spline: BSpline2D, find_trailing_edge=False, verbose=False) -> tuple[np.ndarray, np.ndarray]:
+    @classmethod
+    def _find_trailing_edge(cls, spline: BSpline2D, find_trailing_edge=False, verbose=False) -> tuple[np.ndarray, np.ndarray]:
         """Finds the trailing edge of the airfoil. To account for cases where
         the trailing edge is ill-defined, or missing, there are two methods.
         First, the trailing edge is found by maximizing the distance from the
@@ -244,26 +247,65 @@ class AirfoilNormalizer:
             if not res1.success or not res2.success:
                 raise RuntimeError(
                     "Failed to find trailing edge. \n" +
-                    f"{str(res1)} \n {str(res2)}"
+                    "Search on upper surface: \n" +
+                    f"{str(res1)} \n" +
+                    "Search on lower surface: \n" +
+                    f"{str(res2)}"
                 )
 
-            start, end = spline.evaluate_at(0), spline.evaluate_at(1)
+            # some verification
+            assert np.all(spline.evaluate_at(0) == spline.control_points[0]), \
+                "The first control point does not coincide with u=0."
+            assert np.all(spline.evaluate_at(1) == spline.control_points[-1]), \
+                "The last control point does not coincide with u=1."
+
+            # spline_start, spline_end = spline.evaluate_at(0), spline.evaluate_at(1)
+            spline_start, spline_end = spline.control_points[0], spline.control_points[-1]
+            res1_TE, res2_TE = spline.evaluate_at(res1.x[0]), spline.evaluate_at(res2.x[0])
+
+            # if the found max x locations coincide with the spline end points,
+            # at least no funky stuff is going on, at most some missing points:
+            if np.all(res1_TE == spline_start) and np.all(res2_TE == spline_end):
+                # if the spline endpoints have the same x-coordinate,
+                # the trailing edge is assumed to be at the midpoint of
+                # the start and end points
+                if spline_start.x == spline_end.x:
+                    trailing_edge = 0.5 * (spline_start + spline_end)
+                    return trailing_edge
+
+                # if the spline endpoints dont have matching x-coordinates,
+                # the spline is incomplete, and the trailing edge is assumed to be
+                # at the maximum x-value found, while the other side is missing
+                # data.
+                else:  # elif abs(spline_start.x - spline_end.x) > 1e-6:
+                    # check we're not dealing with a degenerate monstrosity
+                    # assert np.all(res1_TE == spline_start), \
+                    #     "The first control point does not coincide with u=0 after optimization."
+                    # assert np.all(res2_TE == spline_end), \
+                    #     "The last control point does not coincide with u=1 after optimization."
+
+                    # first fix the trailing edge
+                    spline = cls._force_trailing_edge_at_x1(spline, target="xmax")
+                    # at this point, the trailing edge should be fine, but imma
+                    # check anyway because trust isssues
+                    assert spline.evaluate_at(0)[0] == spline.evaluate_at(-1)[0], \
+                        "trailing edge does not match for upper and lower surface."
+                    spline_start, spline_end = spline.control_points[0], spline.control_points[-1]
+                    trailing_edge = 0.5 * (spline_start + spline_end)
+                    return trailing_edge
 
             # if x-locations are not the same, trailing edge is assumed to be
             # at the maximum x-value found, while the other side is missing data
-            if abs(start[0] - end[0]) > 1e-5:
+            elif abs(res1_TE.x - res2_TE.x) > 1e-6:
                 u_te = res1.x[0] if -res1.fun > -res2.fun else res2.x[0]
                 trailing_edge = spline.evaluate_at(u_te)
-                return trailing_edge #, u_te
-            elif abs(start[0] - end[0]) < 1e-5:
-                # if the maximum x values found are the same, the trailing edge
-                # is assumed to be at the midpoint of the start and end points
-                trailing_edge = 0.5 * (start + end)
+                return trailing_edge
+
             else:
-                # this is probably never reached
+                # this is probably never reached. please let it never be reached.
                 raise ValueError(
                     f"Unable to determine trailing edge. \n" +
-                    f"Start: {start}, End: {end}, u1: {res1.x[0]}, u2: {res2.x[0]} \n" +
+                    f"Start: {spline_start}, End: {spline_end}, u1: {res1.x[0]}, u2: {res2.x[0]} \n" +
                     f"{res1} \n {res2}"
                     )
 
@@ -289,9 +331,127 @@ class AirfoilNormalizer:
             # if argument says not to find trailing edge, this whole ordeal is
             # skipped and the trailing edge is assumed (hoped) to be the
             # midpoint of the defined end points.
-            trailing_edge = 0.5*(spline.evaluate_at(0) + spline.evaluate_at(1))
-            trailing_edge[0] = np.min([spline.evaluate_at(0)[0], spline.evaluate_at(1)[0]])
+            spline = cls._force_trailing_edge_at_x1(spline, target="xmax")
+            trailing_edge = 0.5 * (spline.evaluate_at(0) + spline.evaluate_at(1))
+            trailing_edge[0] = np.max([spline.evaluate_at(0)[0], spline.evaluate_at(1)[0]])
         return trailing_edge
+
+    @staticmethod
+    def _force_trailing_edge_at_x1(spline: BSpline2D, target: str = "xmax"):
+        """
+        Forces the trailing edge of the spline to be at x=1. This is done by
+        adjusting the last control point to match the x-coordinate of the
+        trailing edge point, which is assumed to be at the end of the spline.
+        Args:
+            curve (BSpline2D): The spline object representing the airfoil.
+            target (str): The target x-coordinate for the trailing edge.
+                - "xmax" to force the trailing edge at the maximum x found
+                - "1" to force trailing edge at x=1.0
+        Returns:
+            BSpline2D: The adjusted spline with the trailing edge at the target
+                x location.
+        """
+        assert target in ["xmax", "1"], \
+            f"Unknown target for trailing edge: {target}. " + \
+            "Must be either 'xmax' or '1'."
+        assert np.all(spline.evaluate_at(0) == spline.control_points[0]), \
+            "The first control point does not coincide with u=0."
+        assert np.all(spline.evaluate_at(1) == spline.control_points[-1]), \
+            "The last control point does not coincide with u=1."
+
+        spline_start, spline_end = spline.control_points[0], spline.control_points[-1]
+        # get the target x-coordinate for the trailing edge:
+        match target:
+            case "xmax":
+                x_trailing_edge = max(spline_start.x, spline_end.x)
+            case "1":
+                x_trailing_edge = 1.0
+
+        # if the spline represents either the top or bottom side of an airfoil,
+        # the first control point is at the leading edge, and should never be
+        # adjusted. Setting it to match the target will ensure it is skipped.
+        # - might be redundant
+        # if spline_start.x == 0.0:
+        #     spline_start = Point2D([x_trailing_edge, spline_start.y])
+        #     assert spline_start.x == x_trailing_edge, \
+        #         "Failed to overwrite start point"
+
+        if spline_end.x == x_trailing_edge:
+            if spline_start.x == x_trailing_edge or spline_start.x == 0.0:
+                # no adjustments needed
+                return spline
+
+        # if the spline endpoints dont have matching x-coordinates,
+        # the spline is incomplete, and the trailing edge is assumed to be
+        # at the maximum x-value found, while the other side is missing data.
+        # if abs(spline_start.x - spline_end.x) > 1e-6:
+
+        adjusted_control_points = spline.control_points
+        # check if first control point needs adjustment
+        if spline_start.x != x_trailing_edge and spline_start.x != 0.0:
+            # if the start point has a smaller x-value, adjust the
+            # first control point to match the x-value of the last.
+            # IMPORTANT!!! on the top side, the curve is defined
+            # backwards in selig format (from trailing to leading edge)
+
+            direction = adjusted_control_points[0] - adjusted_control_points[1]
+            magnitude = (x_trailing_edge - adjusted_control_points[0].x) / direction[0]
+            adjusted_control_points[0] += magnitude * direction
+
+            # if the surfaces overlap after adjustment, meaning the upper point
+            # was moved down too much, it is moved back up to match the lower
+            # surface, keeping the x-coordinate the same.
+            if adjusted_control_points[0].y < adjusted_control_points[-1].y:
+                adjusted_control_points[0][1] = adjusted_control_points[-1][1]
+
+        # check if last control point needs adjustment
+        if spline_end.x != x_trailing_edge:
+            # if the end point has a smaller x-value, the trailing edge
+            # is on the start point side, so we adjust the last control
+            # point to match the x-value of the first.
+            direction = adjusted_control_points[-1] - adjusted_control_points[-2]
+            magnitude = (x_trailing_edge - adjusted_control_points[-1].x) / direction[0]
+            adjusted_control_points[-1] += magnitude * direction
+
+            # if the surfaces overlap after adjustment, the adjusted control
+            # point is moved to the trailing edge point.
+            if adjusted_control_points[-1].y > adjusted_control_points[0].y:
+                adjusted_control_points[-1][1] = adjusted_control_points[0][1]
+
+        # Finally, if the output should be a normalized spline, it is assumed
+        # tne provided spline was already normalized. Following the potential
+        # adjustemts above, the trailing edge is forced to be mirrored.
+        # This is only done if the target is 1, and if the spline does not
+        # start at the leading edge.
+        if x_trailing_edge == 1.0 and target == "1" and spline_start.x != 0.0 and \
+                adjusted_control_points[0].y != -adjusted_control_points[-1].y:
+            # difference = adjusted_control_points[0].y + adjusted_control_points[-1].y
+            # adjusted_control_points[0][1] -= difference / 2
+            # adjusted_control_points[-1][1] += difference / 2
+            adjusted_control_points[-1][1] = -adjusted_control_points[0][1]
+
+        spline.control_points = adjusted_control_points
+
+        # verify that the adjustement worked
+        assert spline.control_points[-1].x == x_trailing_edge, \
+            f"The last control points do not have the required x-coordinate after adjustment:\n" + \
+            f"Last: {spline.control_points[-1]} instead of x={x_trailing_edge}."
+
+        if spline_start.x != 0.0:
+            assert spline.control_points[0].x == x_trailing_edge, \
+                f"The first control point does not have have the required x-coordinate after adjustment:\n" + \
+                f"Last: {spline.control_points[0]} instead of x={x_trailing_edge}."
+            if target == "1":
+                assert spline.control_points[0].y == -spline.control_points[-1].y, \
+                    "The first and last control points do not have the same y-coordinate after adjustment. "
+
+        # Also verify that the adjusted control points are valid:
+        assert np.all(spline.evaluate_at(0) == spline.control_points[0]), \
+            "The first control point does not coincide with u=0 after adjustment."
+        assert np.all(spline.evaluate_at(1) == spline.control_points[-1]), \
+            "The last control point does not coincide with u=1 after adjustment."
+
+        return spline
 
     @staticmethod
     def _find_leading_edge(spline: BSpline2D, trailing_edge: Point2D) -> tuple[np.ndarray, np.ndarray]:

src/numfoil/geometry/spline.py
@@ -89,7 +89,7 @@ class Curve(ABC):
         curvature = self.curvature_at(x)
         return np.where(curvature != 0, 1 / curvature, np.inf)
 
-    def plot(self, num_points: int = 200) -> None:
+    def plot(self, num_points: int = 2000) -> None:
         """Plot the curve using matplotlib."""
         plt.plot(*self.evaluate_at(np.linspace(0,1,num_points)).T)
         plt.xlabel("X")